05c1e1a500a4b018eb39d536c4206cb46bdbccdb
Your Name <youremail@domain.com>
2024-09-20 01:21:54 +0300

    create objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+    name : "Sam",
+    streetAddress : "123 Nairobi"
+};
+
+//non destructive code
+function updateEmployeeWithKeyAndValue(employee, key , value) {
+
+    return  {
+        ...employee,
+        [key]: value
+    };
+};
+
+//destructive code
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//deletes the first non destructive code
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: _, ...newEmployee } = employee;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
